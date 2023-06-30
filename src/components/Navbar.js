22379f698feba71842c93ac057f944598e7cc10f
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-06-30 22:37:26 +0530

    Navbar cross item created

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,21 +1,42 @@
 import { Component } from "react";
 import "./NavbarStyles.css"
+import { MenuItems } from "./MenuItems";
+import Logo from "../Assets/logoname.png"
 
 
 
 class Navbar extends Component {
+    state = { clicked: false }
+    handleClick = () => {
+        this.setState({ clicked: !this.state.clicked })
+    }
+
+
   render(){
     return (
       <nav className="NavbarItems">
-        <h1 className="navbar-logo"> CodeRunner  </h1>
+        {/* <h1 className="navbar-logo"> CodeRunner  </h1> */}
+        <img src={Logo} alt="Logo" className="navbar-logo"  />
+
+        <div className="menu-icon" onClick={this.handleClick}>
+            <i className={this.state.clicked ? "fas fa-times" : "fas fa-bars"}></i>
+            
+        </div>
 
         <ul className="nav-menu">
-          <li>
-            <a href="/">
-                <i className="fa-solid fa-house"></i>
-                Home
-            </a>
-          </li>
+            {MenuItems.map((item, index) => {
+                return (
+                    <li key={index}>
+                    <a className={item.cName} href={item.url}>
+                        <i className={item.icon}></i>
+                        {item.title} 
+                    </a>
+                  </li>
+                )
+                
+            
+            })}
+         
 
 
         </ul>
