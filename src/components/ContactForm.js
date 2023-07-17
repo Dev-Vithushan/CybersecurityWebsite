f855d9fe2f718a3aa9a1efc3626426e0e210fe32
Vithushan Logeswaran <2018csc042@univ.jfn.ac.lk>
2023-07-17 13:10:37 +0530

    EmailJS Code added to the base cod

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,15 +1,40 @@
+import React, { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import "./ContactFormStyles.css"
 import Button from "./Button"
 
 function ContactForm() {
+
+    const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs.sendForm('service_qrddnai', 'template_cqj8ipb', form.current, 'l9M2SgbqrpRVdNZ4J')
+      .then((result) => {
+          console.log(result.text);
+          alert("Your Message has been sent successfully")
+          
+    
+            var frm = document.getElementsByName('ContactForm')[0];
+            frm.submit(); 
+            frm.reset();  
+            return false; 
+
+
+      }, (error) => {
+          console.log(error.text);
+      });
+    }
+    
     return(
         <div className="form-container">
             <h1> Contact Us</h1>
-            <form>
-                <input type="text" placeholder="Name" required/>
-                <input type="email" placeholder="Email" required/>
-                <input type="text" placeholder="Subject" required/>
-                <textarea placeholder="Message" rows="4" required></textarea>
+            <form name='ContactForm' ref={form} onSubmit={sendEmail}>
+                <input type="text" name="user_name" placeholder="Name" required/>
+                <input type="email" name="user_email" placeholder="Email" required/>
+                <input type="text" name='subject' placeholder="Subject" required/>
+                <textarea name="message" placeholder="Message" rows="4" required></textarea>
                 <button className="buttonClass" type="submit"> Send Message</button>
             </form>
         </div>
